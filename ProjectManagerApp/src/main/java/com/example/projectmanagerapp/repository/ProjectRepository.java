3ac3696f2e241c64118c37bb91b032766388f21b
Kacper Topór <kacpertopor4@gmail.com>
2026-03-11 21:02:34 +0100

    Created an interface for ProjectRepository.Java and added annotations

---
+++ ProjectManagerApp/src/main/java/com/example/projectmanagerapp/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.example.projectmanagerapp.repository;
+
+import com.example.projectmanagerapp.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
