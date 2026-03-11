9f1f36b80f827222594943ae2b32ddde9550da91
sanocki <hubert.sanocki2@gmail.com>
2026-03-11 20:00:02 +0100

    project init

---
+++ ProjectManagerApp/src/main/java/com/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerAppApplication.class, args);
+    }
+
+}
